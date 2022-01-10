a9a8e613acd84fcb1237c20c8f9356f21c962bf0
Veselin Mladenov <Ambroza8888@gmail.com>
2022-01-10 16:27:56 +0200

    added folder structure, Material UI and also components

---
+++ client/src/app/layout/App.tsx
@@ -0,0 +1,26 @@
+import { ThemeProvider } from "@emotion/react";
+import { Container, createTheme, CssBaseline } from "@mui/material";
+import { useState } from "react";
+import Catalog from "../../features/catalog/Catalog";
+import Header from "./Header";
+
+function App() {
+  const [darkMode, setDarkMode] = useState(false);
+  const paletteType = darkMode ? 'dark' : 'light';
+  const theme = createTheme({
+    palette: {
+      mode: paletteType
+    }
+  })
+  return (
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <Header />
+      <Container>
+        <Catalog />
+      </Container>
+    </ThemeProvider>
+  );
+}
+
+export default App;
