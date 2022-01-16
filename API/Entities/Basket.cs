d689d4f6e2c5c65329617d392888a6886ce9e6c0
Veselin Mladenov <Ambroza8888@gmail.com>
2022-01-16 03:22:22 +0200

    added basket fucntionality on the API - basket controler with getBasket end-point createBasked endpont, deleteBasket end-point, also entity and Dto to return objects in the right order, added new migration on Basket and also navigation properties to BasketItem and Product

--- API/Entities/Basket.cs
+++ API/Entities/Basket.cs
@@ -24,7 +24,7 @@ namespace API.Entities
             var item = Items.FirstOrDefault(item => item.ProductId == productId);
             if (item == null) return;
             item.Quantity -= quantity;
-            if (item.Quantity == 0) Items.Remove(item);
+            if (item.Quantity < 1) Items.Remove(item);
         }
     }
 }
