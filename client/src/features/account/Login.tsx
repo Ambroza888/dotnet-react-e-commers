e1e0ee6bf075eca15b264422e482e147bfddc7d9
Veselin Mladenov <Ambroza8888@gmail.com>
2022-02-16 02:36:18 +0200

    added and play around with handleInputChange method.
    React does not come with specifc form, so we need  to hadnle on input on change.

--- client/src/features/account/Login.tsx
+++ client/src/features/account/Login.tsx
@@ -9,18 +9,23 @@ import Typography from '@mui/material/Typography';
 import Container from '@mui/material/Container';
 import { Paper } from '@mui/material';
 import { Link } from 'react-router-dom';
+import { useState } from 'react';
 
 
 export default function Login() {
-  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
-    event.preventDefault();
-    const data = new FormData(event.currentTarget);
-    // eslint-disable-next-line no-console
-    console.log({
-      email: data.get('email'),
-      password: data.get('password'),
-    });
+  const [values, setValues] = useState({
+    username: '',
+    password: ''
+  });
+
+  const handleSubmit = () => {
+    console.log(values);
   };
+
+  const handleInputChange = (event: any) => {
+    const { name, value } = event.target;
+    setValues({ ...values, [name]: value })
+  }
 
   return (
       <Container component={Paper} maxWidth="sm" 
@@ -31,26 +36,24 @@ export default function Login() {
           <Typography component="h1" variant="h5">
             Sign in
           </Typography>
-          <Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
+          <Box component="form" onSubmit={ handleSubmit } noValidate sx={{ mt: 1 }}>
             <TextField
               margin="normal"
-              required
               fullWidth
-              id="email"
-              label="Email Address"
-              name="email"
-              autoComplete="email"
+              label="Username"
+              name="username"
               autoFocus
+              onChange={ handleInputChange }
+              value={ values.username }
             />
             <TextField
               margin="normal"
-              required
               fullWidth
               name="password"
               label="Password"
               type="password"
-              id="password"
-              autoComplete="current-password"
+              onChange={ handleInputChange }
+              value={ values.password }
             />
             <Button
               type="submit"
