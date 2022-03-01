60823c4c85c63d0c74daa56e72f47f2982d9ed50
Veselin Mladenov <Ambroza8888@gmail.com>
2022-03-01 17:27:42 +0200

    spacing

--- API/Entities/Basket.cs
+++ API/Entities/Basket.cs
@@ -8,11 +8,11 @@ namespace API.Entities
         public int Id { get; set; }
         public string BuyerId { get; set; }
         public List<BasketItem> Items { get; set; } = new List<BasketItem>();
-        public void AddItem (Product product, int quantity)
+        public void AddItem(Product product, int quantity)
         {
             if (Items.All(item => item.ProductId != product.Id))
             {
-                Items.Add(new BasketItem{Product = product, Quantity = quantity});
+                Items.Add(new BasketItem { Product = product, Quantity = quantity });
             }
 
             var existingItem = Items.FirstOrDefault(item => item.ProductId == product.Id);
