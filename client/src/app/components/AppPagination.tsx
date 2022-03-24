5ca401edc5d660743502bcbb73b41c3c9b885fea
Veselin Mladenov <Ambroza8888@gmail.com>
2022-03-24 11:24:51 +0200

    added Inventory page.

--- client/src/app/components/AppPagination.tsx
+++ client/src/app/components/AppPagination.tsx
@@ -1,4 +1,5 @@
 import { Box, Pagination, Typography } from "@mui/material";
+import { useState } from "react";
 import { MetaData } from "../models/pagination";
 
 interface Props {
@@ -7,6 +8,12 @@ interface Props {
 }
 export default function AppPagination({ metaData, onPageChange }: Props) {
     const { currentPage, totalCount, totalPages, pageSize } = metaData;
+    const [pageNumber, setPageNumber] = useState(currentPage);
+
+    const handlePageChange = (page: number) => {
+      setPageNumber(page);
+      onPageChange(page);
+    }
   return (
     <Box display="flex" justifyContent="space-between" alignItems="center">
       <Typography>Displaying { (currentPage - 1) * pageSize + 1 }-
@@ -17,8 +24,8 @@ export default function AppPagination({ metaData, onPageChange }: Props) {
         color="secondary"
         size="large"
         count={ totalPages }
-        page={ currentPage }
-        onChange={(e, page) => onPageChange(page)}
+        page={ pageNumber }
+        onChange={(e, page) => handlePageChange(page)}
       />
     </Box>
   );
